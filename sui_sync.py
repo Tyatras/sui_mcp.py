869b3fa93f767210de9fbb2e476ffa60a0cf767b
Tyatras <theain786@gmail.com>
2025-06-11 16:54:05 -0400

    Update sui_sync.py

--- sui_sync.py
+++ sui_sync.py
@@ -3,7 +3,7 @@ from google.oauth2.service_account import Credentials
 import requests
 
 # Setup Google Sheets credentials
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 creds = Credentials.from_service_account_file("service_account.json", scopes=SCOPES)
 client = gspread.authorize(creds)
 
